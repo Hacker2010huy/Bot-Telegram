06e7fdf245a96b7944592897acae414d5a82b240
Hacker2010huy <142239707+Hacker2010huy@users.noreply.github.com>
2025-02-24 21:55:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import telebot
+from keep_alive import keep_alive
+keep_alive()
 
+BOT_TOKEN = "7399774608:AAExFx2rNd_LYCBfTHGDMQWpzBFs7JFFm4U"
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Xin chào! Tôi là bot Telegram của bạn.")
+
+bot.infinity_polling()
