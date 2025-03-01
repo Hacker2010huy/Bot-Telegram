a1b8a64776083e77b0ab1053fe5dad550e03e160
Hacker2010huy <142239707+Hacker2010huy@users.noreply.github.com>
2025-03-01 16:28:54 +0700

    Update stop.py

--- stop.py
+++ stop.py
@@ -1,4 +1,4 @@
-import telebot,os,time,requests
+import telebot,os
 from keep_alive import keep_alive
 keep_alive()
 print("Bot offline")
@@ -9,9 +9,6 @@ ADMIN_ID = [6481553299]
 @bot.message_handler(commands=['onbot'])
 def onbot(message):
     if (message.from_user.id in ADMIN_ID):
-    	text_code = requests.get("https://raw.githubusercontent.com/Hacker2010huy/Bot-Telegram/main/main.py").text
-    	with open("main.py", "w", encoding="utf-8") as f:
-        	f.write(text_code)
         bot.reply_to(message, "Bot đã bật🔋")
         bot.stop_polling()
 
