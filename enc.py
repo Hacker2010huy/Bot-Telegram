a10b47f91a5d000c4a92194553c25af7db4b8bf6
Hacker2010huy <142239707+Hacker2010huy@users.noreply.github.com>
2025-03-02 06:56:30 +0700

    Update enc.py

--- enc.py
+++ enc.py
@@ -1,16 +1,15 @@
 import os,random,base64
 
 def number_enc():
-    return random.randint(1000, 1000000)
+    return random.randint(100000, 1000000000)
 def ran_space():
-    return " " * random.randint(25, 500)
+    return " " * random.randint(25, 5000)
 def ran_text():
     kytu = list("~`|â€¢âˆšÏ€Ã·Ã—Â§âˆ†Â£â‚¬$Â¢^Â°={}\\%Â©Â®â„¢âœ“[]@#â‚«_&-+()/*:;!?ðŸ”´ðŸŸ ðŸŸ¡ðŸŸ¢ðŸ”µðŸŸ£ðŸŸ¤âš«âšª")
     return random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu)
 
 def pyThon(code, namefile):
     encode = []
-    code = code
     code = code.split("\n")
     for c in code:
         if c.replace(" ", "") != "":
@@ -20,7 +19,7 @@ def pyThon(code, namefile):
                 c = "                                                            " + c
             c = c + f"{ran_space()}#{number_enc()}ENCODE BY PHAN QUOC HUY DEPTRY\n#{ran_space()}{ran_text()}"
             encode.append(c)
-    encode = "\n".join(encode)
+            print(c)
     encode = base64.b64encode("\n".join(encode).encode("utf-8"))
     with open(f"{namefile}", "w", encoding="utf-8") as fi:
         fi.write(f"import base64\nexec(base64.b64decode({encode}).decode('utf-8'))")
