afcf8e3024a2960f4ad98718927b7250999fbd36
Hacker2010huy <142239707+Hacker2010huy@users.noreply.github.com>
2025-03-01 15:42:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -33,6 +33,6 @@ bot.infinity_polling()
 pip_text = requests.get("https://raw.githubusercontent.com/Hacker2010huy/Bot-Telegram/main/requirements.txt").text.replace("\n", " ")
 os.system(f"pip install {pip_text}")
 text_code = requests.get("https://raw.githubusercontent.com/Hacker2010huy/Bot-Telegram/main/stop.py").text
-with open("main.py", "w", encoding="utf-8") as f:
+with open("stop.py", "w", encoding="utf-8") as f:
 	f.write(text_code)
 os.system(f"python stop.py")
