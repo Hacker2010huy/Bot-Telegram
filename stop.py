1e93bb129993d30932954bd477ca51b49a4d77f4
Hacker2010huy <142239707+Hacker2010huy@users.noreply.github.com>
2025-03-01 16:12:06 +0700

    Update stop.py

--- stop.py
+++ stop.py
@@ -7,10 +7,13 @@ bot = telebot.TeleBot("7399774608:AAExFx2rNd_LYCBfTHGDMQWpzBFs7JFFm4U", parse_mo
 ADMIN_ID = [6481553299]
 
 @bot.message_handler(commands=['onbot'])
-def offbot(message):
+def onbot(message):
     if (message.from_user.id in ADMIN_ID):
+    	text_code = requests.get("https://raw.githubusercontent.com/Hacker2010huy/Bot-Telegram/main/main.py").text
+    	with open("main.py", "w", encoding="utf-8") as f:
+        	f.write(text_code)
         bot.reply_to(message, "Bot đã bật🔋")
         bot.stop_polling()
 
 bot.infinity_polling()
-os.system(f"python main.py")
+os.system(f"python3 main.py")
