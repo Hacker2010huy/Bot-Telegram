ec58e4bd99abcb27e0c7f00caee05951cdc46b3c
Hacker2010huy <142239707+Hacker2010huy@users.noreply.github.com>
2025-03-01 14:30:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,30 @@ import telebot,os,time
 from keep_alive import keep_alive
 keep_alive()
 
-BOT_TOKEN = "7399774608:AAExFx2rNd_LYCBfTHGDMQWpzBFs7JFFm4U"
-botRunning = True
-
-bot = telebot.TeleBot(BOT_TOKEN)
+bot = telebot.TeleBot("7384250892:AAHKP4H-Bbao773rN_oxF9lp41W3OGoF_m8", parse_mode="HTML")
+file_acc = "pqhsave.json"
+ADMIN_ID = [6481553299]
 
 @bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, "Xin chào! Tôi là bot Telegram của bạn.")
+def ds_lenh(message):
+	text = '''
+PHAN QUOC HUY BOT
+=================
+Danh sách các lệnh
+=================
+/help - Xem lệnh
+/tx - comming soon
+ADMIN:
+/offbot - Tắt bot
+/onbot - Bật bot
+'''
+	bot.reply_to(message, text);
+
+@bot.message_handler(commands=['offbot'])
+def offbot(message):
+    if (message.from_user.id in ADMIN_ID):
+        bot.reply_to(message, "Bot đã tắt🪫")
+        bot.stop_polling()
 
 bot.infinity_polling()
+os.system("python stop.py")
