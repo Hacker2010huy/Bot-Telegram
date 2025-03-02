5c881b9bfce9d9dbd66e32b56ca39c7668237f3b
Hacker2010huy <142239707+Hacker2010huy@users.noreply.github.com>
2025-03-02 08:26:24 +0700

    Update enc.py

--- enc.py
+++ enc.py
@@ -1,12 +1,12 @@
 import os,random,base64
 
 def number_enc():
-    return random.randint(100000, 1000000000)
+    return random.randint(1000, 1000000)
 def ran_space():
-    return " " * random.randint(25, 5000)
+    return " " * random.randint(2000, 50000)
 def ran_text():
-    kytu = list("~`|â€¢âˆšÏ€Ã·Ã—Â§âˆ†Â£â‚¬$Â¢^Â°={}\\%Â©Â®â„¢âœ“[]@#â‚«_&-+()/*:;!?ðŸ”´ðŸŸ ðŸŸ¡ðŸŸ¢ðŸ”µðŸŸ£ðŸŸ¤âš«âšª")
-    return random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu)
+    kytu = list("QWERTYUIOPASDFGHJKLZXCVBNM~`|â€¢âˆšÏ€Ã·Ã—Â§âˆ†Â£â‚¬$Â¢^Â°={}\\%Â©Â®â„¢âœ“[]@#â‚«_&-+()/*:;!?ðŸ”´ðŸŸ ðŸŸ¡ðŸŸ¢ðŸ”µðŸŸ£ðŸŸ¤âš«âšªqwertyuiopasdfghjklzxcvbnm1234567890")
+    return random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu) + random.choice(kytu)
 
 def pyThon(code, namefile):
     encode = []
@@ -16,10 +16,9 @@ def pyThon(code, namefile):
             if c.strip().startswith("#"):
                 continue
             if c.startswith(" "):
-                c = "                                                            " + c
-            c = c + f"{ran_space()}#{number_enc()}ENCODE BY PHAN QUOC HUY DEPTRY\n#{ran_space()}{ran_text()}"
+                c = " " * 250 + c
+            c = c + f"{ran_space()}#{number_enc()}ENCODE BY PHAN QUOC HUY DEPTRY\n#DÒNG NÀY QUAN TRỌNG KHÔNG ĐƯỢC XOÁ{ran_space()}{ran_text()}"
             encode.append(c)
-            print(c)
     encode = base64.b64encode("\n".join(encode).encode("utf-8"))
     with open(f"{namefile}", "w", encoding="utf-8") as fi:
         fi.write(f"import base64\nexec(base64.b64decode({encode}).decode('utf-8'))")
